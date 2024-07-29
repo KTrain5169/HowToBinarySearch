bdb1ff6fd9e44730fbcfc50f1afe57e854d435c0
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-07-29 19:42:54 +1000

    add: more portions of the script

--- binary_search.py
+++ binary_search.py
@@ -11,8 +11,35 @@ needhelp = input(str("Do you still need help performing a binary search? "))
 if needhelp.lower() == "yes":
     print("OK! Let's start. Tell me what your issue is and I'll repeat that to you every step.")
     issue = input("What's your issue? ")
-    print('Ok, set your issue to ', issue, '".')
-    print("So let's start!")
+    print('Ok, set your issue to "', issue, '".')
+    print("So let's start!\n")
+
+    print("Let's start with the first step. Try removing everything that you're testing with (ex. mods, addons, whatever).")
+    problem = input(str("Does your problem persist?"))
+    if problem.lower() == "yes":
+        print("This is unfortunately a problem with the base software. Please contact the vendor to figure out fixes.")
+        abort = input(str("Type EXIT to exit. "))
+        if abort.upper() == "EXIT":
+            exit()
+    elif problem.lower() == "no":
+        problem = ''
+        print("Great, we can rule out the possibility of it being a base software problem.")
+        print("Next, we'll add back half the testing items.")
+        print('Also, make sure that dependencies of each testing item is included.\nIf your list of testing items is long, you can simply consider that things such as libraries always stay, this is because they are the least likely to cause issues.\n')
+        print('As a reminder, your issue was "', issue, '".\n')
+        problem = input(str('Does your issue persist?'))
+        if problem.lower() == "yes":
+            while var1 > 1:
+                problem = ''
+                print("Ok, let's halve the count of testing items further. ")
+                print('As a reminder, your issue was "', issue, '".\n')
+                problem = input(str('Does your issue persist?'))
+                if problem.lower() == "yes":
+                    break
+                elif problem.lower() == "no":
+                    break
+        elif 
+
     
 elif needhelp.lower() == "no":
     print("Alright, I'll leave you to it.")
@@ -22,6 +49,6 @@ elif needhelp.lower() == "no":
         exit()
 else:
     print("Uh... can't understand you.")
-    abort = input(str("Type EXIT to abort. "))
+    abort = input(str("Type EXIT to abort. Run the program again to restart. "))
     if abort.upper() == "EXIT":
         exit()
